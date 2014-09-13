a8545c452fcf0ce874ad9965a9a6daa9f45f4c84
Robert Timm <bajabob.tx@gmail.com>
2014-09-13 17:51:15 -0400

    update

--- web_includes/application/controllers/ApiController.php
+++ web_includes/application/controllers/ApiController.php
@@ -16,7 +16,7 @@ class ApiController extends Zend_Controller_Action
     {
     	echo json_encode(array("actionable" => "Hydrate", 
     							"icon" => "water", 
-    							"message" => "Jim, you need to make sure you drink some water.",
+    							"message" => "Hi eleni!.",
     							"show_timestamp" => "1410590293",
     							"show_time"	=> date("F j, Y, g:i a", "1410590293")));
     	die;
