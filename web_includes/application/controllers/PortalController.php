c550b89782249086e6ec5eb0dcf064880dc79f17
Robert Timm <bajabob.tx@gmail.com>
2014-09-14 05:24:07 -0400

    update

--- web_includes/application/controllers/PortalController.php
+++ web_includes/application/controllers/PortalController.php
@@ -26,6 +26,29 @@ class PortalController extends Zend_Controller_Action
 		*/
 		if($request->isPost())
 		{
+			
+			$act = new Application_Model_Actionables();
+			
+			for($i = 0; $i < 10; $i++)
+			{
+				if(isset($_POST['medication_'.$i]))
+				{
+					$arr = array(
+						'actionable' 	=> "Medication",
+						'icon'			=> 'pill',
+						'message'		=> 'Time to take your '.$_POST['medication_'.$i]."!"
+					);
+					$act->create(json_encode($arr));
+				}
+			}
+			
+			$arr = array(
+				'actionable' 	=> "Activity",
+				'icon'			=> 'exercise',
+				'message'		=> $_POST['activity']
+			);
+			$act->create(json_encode($arr));
+			
 			Zend_Debug::dump($_POST);die;
 		}
 	}
